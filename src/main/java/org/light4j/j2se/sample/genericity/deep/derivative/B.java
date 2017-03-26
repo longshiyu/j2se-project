bde4d779dd1d7b976250e62902770bdbfcf6df72
longjiazuo <812198178@qq.com>
2017-03-26 19:43:07 +0800

    add derivative genericity subclass

---
+++ src/main/java/org/light4j/j2se/sample/genericity/deep/derivative/B.java
@@ -0,0 +1,8 @@
+package org.light4j.j2se.sample.genericity.deep.derivative;
+
+import org.light4j.j2se.sample.genericity.deep.define.Apple;
+
+////使用Apple类时，没有为T传入实际的类型参数
+public class B extends Apple {
+
+}
